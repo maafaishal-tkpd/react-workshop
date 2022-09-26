da346fcec5cb91ca42a178db76a9e35300922257
alanis.faishal <alanis.faishal@tokopedia.com>
2022-09-26 20:23:35 +0700

    chore: server

--- src/server.js
+++ src/server.js
@@ -5,8 +5,6 @@ import { StaticRouter } from 'react-router-dom';
 import { renderToString } from 'react-dom/server';
 import { ServerStyleSheet } from 'styled-components';
 import express from 'express';
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
-import { getDataFromTree } from '@apollo/client/react/ssr';
 
 const razzleAssets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -15,7 +13,7 @@ const sheet = new ServerStyleSheet();
 const cssLinksFromAssets = (assets, entrypoint) => {
   return assets[entrypoint]
     ? assets[entrypoint].css
-      ? assets[entrypoint].css.map((asset) => <link rel="stylesheet" href={asset} />)
+      ? assets[entrypoint].css.map((asset, idx) => <link key={idx} rel="stylesheet" href={asset} />)
       : ''
     : '';
 };
@@ -23,7 +21,7 @@ const cssLinksFromAssets = (assets, entrypoint) => {
 const jsScriptTagsFromAssets = (assets, entrypoint) => {
   return assets[entrypoint]
     ? assets[entrypoint].js
-      ? assets[entrypoint].js.map((asset) => <script src={`${asset}`} defer crossOrigin />)
+      ? assets[entrypoint].js.map((asset, idx) => <script key={idx} src={`${asset}`} defer crossOrigin />)
       : ''
     : '';
 };
@@ -34,54 +32,35 @@ server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
-    const apolloClient = new ApolloClient({
-      ssrMode: true,
-      cache: new InMemoryCache(),
-      link: createHttpLink({
-        uri: 'https://asia-southeast2-minitokopedia.cloudfunctions.net/graphql',
-      }),
-    });
-
     const context = {};
     const markup = (
-      <ApolloProvider client={apolloClient}>
-        <StaticRouter context={context} location={req.url}>
-          <App />
-        </StaticRouter>
-      </ApolloProvider>
+      <StaticRouter context={context} location={req.url}>
+        <App />
+      </StaticRouter>
     );
 
     if (context.url) {
       res.redirect(context.url);
     } else {
-      getDataFromTree(markup).then((content) => {
-        const initialState = apolloClient.extract();
+      const html = renderToString(
+        sheet.collectStyles(
+          <html lang="en">
+            <head>
+              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+              <meta charset="utf-8" />
+              <title>Welcome to Razzle</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              {cssLinksFromAssets(razzleAssets, 'client')}
+            </head>
+            <body>
+              <div id="root">${markup}</div>
+              {jsScriptTagsFromAssets(razzleAssets, 'client')}
+            </body>
+          </html>,
+        ),
+      );
 
-        const html = renderToString(
-          sheet.collectStyles(
-            <html lang="en">
-              <head>
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <meta charset="utf-8" />
-                <title>Welcome to Razzle</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                {cssLinksFromAssets(razzleAssets, 'client')}
-              </head>
-              <body>
-                <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
-                <script
-                  dangerouslySetInnerHTML={{
-                    __html: `window.__APOLLO_STATE__=${JSON.stringify(initialState).replace(/</g, '\\u003c')};`,
-                  }}
-                />
-                {jsScriptTagsFromAssets(razzleAssets, 'client')}
-              </body>
-            </html>,
-          ),
-        );
-
-        res.status(200).header('Content-Type', 'text/html').send(`<!doctype html>\n${html}`);
-      });
+      res.status(200).header('Content-Type', 'text/html').send(`<!doctype html>\n${html}`);
     }
   });
 
